1cca0a55b2592e0d37a36eb86c519fe77b2cda74
hq <hq_cml@163.com>
2015-07-20 21:28:08 +0800

    php file

--- php_module_dlist.php
+++ php_module_dlist.php
@@ -1,8 +1,12 @@
 <?php
     $list = dlist_create();
-    dlist_add_head($list, "a");
-    dlist_add_head($list, "b");
-    dlist_add_head($list, "c");
+    $a = "a";
+    $b = "b";
+    $c = "c";
+    dlist_add_head($list, $a);
+    dlist_add_head($list, $b);
+    dlist_add_head($list, $c);
+
     
     
     $length = dlist_element_nums($list);
@@ -11,4 +15,7 @@
     
     echo "The Content of dlist:<br>";
     for($i = 0; $i < $length; $i++)
-        echo dlist_fetch_index($list, $i)." ";
+    {
+        var_dump( dlist_fetch_index($list, $i));
+        echo "<br>";
+    }
