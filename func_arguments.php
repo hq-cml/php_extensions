2fa57d83329c06c6f5c1b635d94599562942da4a
hq <hq_cml@163.com>
2016-04-16 15:49:14 +0800

    调整名字

--- func_arguments.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "<pre>";
-    new_getlong(10);
-    new_getlong("10");
-    //new_getlong("abc"); #PHP Warning:  new_getlong() expects parameter 1 to be long
-    get_str("HAHA");
-    get_str(12345);
-    new_hello_world("HAHA", "World");
-    new_hello_world("HAHA");
--- func_arguments/config.m4
+++
@@ -1,8 +0,0 @@
-PHP_ARG_ENABLE(hq,
-    [Whether to enable the "hq" extension],
-    [  enable-hq        Enable "hq" extension support])
-
-if test $PHP_HQ != "no"; then
-    PHP_SUBST(HQ_SHARED_LIBADD)
-    PHP_NEW_EXTENSION(hq, hq.c, $ext_shared)
-fi
