d83c3d84133c84080c249ea1992393a8c85209d5
hq <hq_cml@163.com>
2016-04-21 08:28:05 +0800

    定义并注册一个接口

--- class/hq.c
+++ class/hq.c
@@ -8,6 +8,8 @@ ZEND_FUNCTION(hq_hello)
 
 //类的句柄
 zend_class_entry *myclass_ce;
+//接口句柄
+zend_class_entry *myinterface_ce;
 
 //定义类方法：ZEND_METHOD，注意区别上面那个叫ZEND_FUNCTION
 ZEND_METHOD( myclass , public_method )
@@ -27,21 +29,32 @@ static zend_function_entry myclass_method[] = {
     ZEND_ME(myclass,    __construct,    NULL,   ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
     { NULL, NULL, NULL }
 };
- 
+//自定义接口的方法列表
+static zend_function_entry myinterface_method[]={
+    ZEND_ABSTRACT_ME(myinterface, hello, NULL) //注意这里的null指的是arginfo
+    {NULL,NULL,NULL}
+};
+
 //MINIT函数，则侧一个类
 ZEND_MINIT_FUNCTION(my_minit_func)
 {
-    zend_class_entry ce;
+    zend_class_entry ce1;
+    zend_class_entry ce2;
      
     //注册一个类，"myclass"是这个类的名称。myclass_method是类的方法列表
-    INIT_CLASS_ENTRY(ce, "myclass",myclass_method);
-    myclass_ce = zend_register_internal_class(&ce TSRMLS_CC);
+    INIT_CLASS_ENTRY(ce1, "myclass",myclass_method);
+    myclass_ce = zend_register_internal_class(&ce1 TSRMLS_CC);
 	
 	//定义属性:$public_var, 默认值null，权限public
     zend_declare_property_null(myclass_ce, "public_var", strlen("public_var"), ZEND_ACC_PUBLIC TSRMLS_CC);
 	
 	//定义常量
 	zend_declare_class_constant_long(myclass_ce, "MY_CONST" , strlen("MY_CONST"), 10 TSRMLS_CC);
+	
+	//注册一个接口    
+	INIT_CLASS_ENTRY(ce2, "myinterface", myinterface_method);
+    myinterface_ce = zend_register_internal_interface(&ce2 TSRMLS_CC);
+	
     return SUCCESS;
 }
 
