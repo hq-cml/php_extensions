f743acfa08582374f01ef57095ff490bfa1e0586
hq <hq_cml@163.com>
2016-04-21 08:20:34 +0800

    comment

--- class.php
+++ class.php
@@ -1,4 +1,20 @@
 <?php
+/*
+class myclass
+{
+    public $public_var;
+    public const MY_CONST=10;
+    public function __construct()
+    {
+        echo"我是__construct方法\n";
+    }
+    public function public_method()
+    {
+        echo"我是public类型的方法\n";
+    }
+}
+*/
+
     $obj= new myclass();
     $obj->public_var = 1;
     $obj->public_method();
