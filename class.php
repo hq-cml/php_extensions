28e64fcb74d2f7320a9ca931ce2cf2aa2f6a6746
hq <hq_cml@163.com>
2016-04-21 08:33:49 +0800

    接口

--- class.php
+++ class.php
@@ -13,6 +13,11 @@ class myclass
         echo"我是public类型的方法\n";
     }
 }
+
+interface myinterface
+{
+    public function hello();
+}
 */
 
     $obj= new myclass();
@@ -20,3 +25,17 @@ class myclass
     $obj->public_method();
     echo "类有一个常量MY_CONST：".myclass::MY_CONST."\n";
     var_dump($obj);
+
+    //接口实现
+    class myclass2 implements myinterface
+    {
+	public $name="hello world!";
+
+	public function hello()
+	{
+		echo $this->name."\n";
+	}
+    }
+
+    $obj= new myclass2();
+    $obj->hello();
