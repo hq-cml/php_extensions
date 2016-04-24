85b229eb7b99b6b0e8c80e9774d71c887ac838a7
hq <hq_cml@163.com>
2016-04-24 22:07:54 +0800

    全局变量声明

--- start_stop/php_hq.h
+++ start_stop/php_hq.h
@@ -2,7 +2,7 @@
 #ifndef HQ_H
 #define HQ_H
  
-// //加载config.h，如果配置了的话
+//加载config.h，如果配置了的话
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -11,5 +11,10 @@
 #include "php.h"
 #define phpext_hq_ptr &hq_module_entry
 extern zend_module_entry hq_module_entry;
+   
+//声明全局变量   
+ZEND_BEGIN_MODULE_GLOBALS(hq)
+    unsigned long counter;     //一个计数器
+ZEND_END_MODULE_GLOBALS(hq)
    
 #endif
