7086aba835f4a84e22584a17aa9cf74bb1976e59
hq <hq_cml@163.com>
2016-04-24 17:51:34 +0800

    MINFO函数

--- start_stop/hq.c
+++ start_stop/hq.c
@@ -24,6 +24,14 @@ PHP_RSHUTDOWN_FUNCTION(hq)
 {
     return SUCCESS;
 }
+ 
+PHP_MINFO_FUNCTION(hq) {
+	php_info_print_table_start();
+    php_info_print_table_row(2, "hq Module", "enabled");
+    php_info_print_table_row(2, "version", "1.0");
+    php_info_print_table_end();
+}
+
 static zend_function_entry hq_functions[] = {
     ZEND_FE(hq_hello,        NULL)
     { NULL, NULL, NULL }
@@ -36,12 +44,12 @@ zend_module_entry hq_module_entry = {
      STANDARD_MODULE_HEADER,
 #endif
     "hq", //这个地方是扩展名称，往往我们会在这个地方使用一个宏。
-    hq_functions, /* Functions */
+    hq_functions,      /* Functions */
     PHP_MINIT(hq),     /* MINIT */
     PHP_MSHUTDOWN(hq), /* MSHUTDOWN */
     PHP_RINIT(hq),     /* RINIT */
     PHP_RSHUTDOWN(hq), /* RSHUTDOWN */
-    NULL, /* MINFO */
+    PHP_MINFO(hq),     /* MINFO */
 #if ZEND_MODULE_API_NO >= 20010901
     "2.1", //这个地方是我们扩展的版本
 #endif
